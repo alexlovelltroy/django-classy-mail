ed4a2a018f6f84142652f22b1bec93ac804c7fef
Alex Lovell-Troy <alex@lovelltroy.org>
2014-02-18 13:39:36 -0600

    Adding the template loader

--- classy_mail/template.py
+++ classy_mail/template.py
@@ -1,4 +1,10 @@
-from django.template import Template, TemplateEncodingError
+from django.template.base import (
+    Template,
+    TemplateDoesNotExist,
+    TemplateEncodingError,
+)
+from django.template.loader import make_origin
+from django.template.loaders.filesystem import Loader as FilesystemLoader
 from markdown import markdown
 import yaml
 #from BeautifulSoup import BeautifulSoup
@@ -22,3 +28,30 @@ class MarkdownTemplate(Template):
         except UnicodeDecodeError:
             raise TemplateEncodingError("Templates can only be constructed "
                                         "from unicode or UTF-8 strings.")
+
+
+class MarkdownTemplateLoader(FilesystemLoader):
+
+    def load_template(self, template_name, template_dirs=None):
+        source, display_name = self.load_template_source(template_name,
+                                                         template_dirs)
+        origin = make_origin(display_name, self.load_template_source,
+                             template_name, template_dirs)
+        try:
+            template = get_template_from_string(source, origin, template_name)
+            return template, None
+        except TemplateDoesNotExist:
+            # If compiling the template we found raises TemplateDoesNotExist,
+            # back off to returning the source and display name for the
+            # template we were asked to load. This allows for correct
+            # identification (later) of the actual template that does not
+            # exist.
+            return source, display_name
+
+
+def get_template_from_string(source, origin=None, name=None):
+    """
+    Returns a compiled Template object for the given template code,
+    handling template inheritance recursively.
+    """
+    return MarkdownTemplate(source, origin, name)
