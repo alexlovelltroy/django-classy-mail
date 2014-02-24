5d1ca40abd974a7d51896ae67842dbdf3596c00c
Alex Lovell-Troy <alex@intros.to>
2014-02-24 22:40:54 +0000

    adding UniqueMarkdownMessage to test Unique funcionality

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-classy-mail',
-    version='0.1.0',
+    version='0.1.2',
     author='Alex Lovell-Troy',
     author_email='alex@lovelltroy.org',
     description='Class-Based Email for Django built with Mixins',
